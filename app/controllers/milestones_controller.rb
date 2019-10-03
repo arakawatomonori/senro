8263da69a333de94703b616f42bed3766e4c9927
uekibati <mtym07083@gmail.com>
2019-10-03 21:42:36 +0900

    コントローラにコメントを追加

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -5,6 +5,7 @@ class MilestonesController < ApplicationController
     @milestones = Milestone.all
     # 拡張子によって出力を切り替える
     respond_to do |format|
+      # /views/milestones/index.html.erb
       format.html
       # 拡張子がjsonだったら、jsonフォーマットで出力
       #　https://ja.wikipedia.org/wiki/JavaScript_Object_Notation
@@ -17,30 +18,34 @@ class MilestonesController < ApplicationController
   def show
     #　Milestoneモデルのidを取得
     @milestone = Milestone.find(params[:id])
+    # /views/milestones/show.html.erb
   end
   
   # GET /milestones
   def new
     # 空のMilestoneモデルを生成
     @milestone = Milestone.new
+    # /views/milestones/new.html.erb
   end
-  
-  #　POST　/milestones
-  def create
-    #　paramsの中の値でMilestoneモデルを生成
-    #　permit ＝　許す　name,distance,photo_urlを許可
-    @milestone = Milestone.new(params.require(:milestone).permit(:name, :distance, :photo_url))
-    # Milestoneモデルを保存
-    @milestone.save
-    #　/milestones/@milestone.id
-    redirect_to @milestone
-  end
-
   
   # GET /milestones/:id/edit 
   def edit
     #　Milestoneモデルのidを取得
     @milestone = Milestone.find(params[:id])
+    # /views/milestones/edit.html.erb
+  end
+
+  #　POST　/milestones
+  def create
+    #　paramsの中の値でMilestoneモデルを生成
+    # require =　必要　milestoneが必須
+    #　permit ＝　許す　name,distance,photo_urlを許可
+    @milestone = Milestone.new(params.require(:milestone).permit(:name, :distance, :photo_url))
+    # Milestoneモデルを保存
+    @milestone.save
+    # redirect 向け直す　
+    #　/milestones/@milestone.id
+    redirect_to @milestone
   end
 
   # PATCH /milestones/:id
@@ -50,8 +55,7 @@ class MilestonesController < ApplicationController
     #　permit ＝　許す　name,distance,photo_urlを許可
     #  既存のMilestoneモデルを上書き
     @milestone.update(params.require(:milestone).permit(:name, :distance, :photo_url))
+    # /milestones
     redirect_to milestones_path
   end
-
-
 end
