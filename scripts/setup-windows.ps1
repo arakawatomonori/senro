dd9a2fd95df32bd7a9fa19c2331a5ff358254245
yuiseki <yuiseki@gmail.com>
2019-10-06 13:49:34 +0900

    add .vscode dir and cSpell.json

--- scripts/setup-windows.ps1
+++ scripts/setup-windows.ps1
@@ -1,6 +1,7 @@
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-cinst ruby
-cinst msys2
+refreshenv
+cinst ruby -y
+cinst msys2 -y
 refreshenv
 ridk install 1 2 3
 gem update --system
