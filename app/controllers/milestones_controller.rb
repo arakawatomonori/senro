ac3103c19575d49cbf9cc9d51e7ac555738c5948
yuiseki <yuiseki@gmail.com>
2019-10-07 13:22:19 +0900

    refactor

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -1,40 +1,34 @@
 class MilestonesController < ApplicationController
-  # GET /milestones.:format
+  # このサーバーに以下のようなリクエストが来たときにレスポンスするコントローラー
+  # GET /milestones HTTP/1.1
+  # GETとかの意味はここ
+  # http://www.tohoho-web.com/ex/http.htm
+  # https://ja.wikipedia.org/wiki/Hypertext_Transfer_Protocol
+
+  # GET /milestones.format
   def index
     #　Milestoneモデルをすべて取得
     @milestones = Milestone.all
-    # 拡張子によって出力を切り替える
+    # URLの拡張子によって出力を切り替える
     respond_to do |format|
+      # formatが空のときはhtmlとされる
       # /views/milestones/index.html.erb
       format.html
       # 拡張子がjsonだったら、jsonフォーマットで出力
+      # http://www.tohoho-web.com/ex/json.html
       #　https://ja.wikipedia.org/wiki/JavaScript_Object_Notation
       format.json { render :json => @milestones}
       format.xml { render :xml => @milestones}
     end
   end
-
-  # GET /milestones/:id
-  def show
-    #　Milestoneモデルのidを取得
-    @milestone = Milestone.find(params[:id])
-    # /views/milestones/show.html.erb
-  end
   
-  # GET /milestones
+  # GET /milestones/new
   def new
     # 空のMilestoneモデルを生成
     @milestone = Milestone.new
     # /views/milestones/new.html.erb
   end
   
-  # GET /milestones/:id/edit 
-  def edit
-    #　Milestoneモデルのidを取得
-    @milestone = Milestone.find(params[:id])
-    # /views/milestones/edit.html.erb
-  end
-
   #　POST　/milestones
   def create
     #　paramsの中の値でMilestoneモデルを生成
@@ -46,6 +40,20 @@ class MilestonesController < ApplicationController
     # redirect 向け直す　
     #　/milestones/@milestone.id
     redirect_to @milestone
+  end
+
+  # GET /milestones/:id
+  def show
+    #　Milestoneモデルのidを取得
+    @milestone = Milestone.find(params[:id])
+    # /views/milestones/show.html.erb
+  end
+
+  # GET /milestones/:id/edit 
+  def edit
+    #　Milestoneモデルのidを取得
+    @milestone = Milestone.find(params[:id])
+    # /views/milestones/edit.html.erb
   end
 
   # PATCH /milestones/:id
@@ -68,6 +76,4 @@ class MilestonesController < ApplicationController
     # /milestones
     redirect_to milestones_path
   end
-  
-
 end
